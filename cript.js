b4eaae7936b67c86fad1bb7d1312664bc234045a
svyas73 <svyas@redolentech.com>
2025-08-10 00:12:51 -0700

    Remove mobile app files - moved to mobile repository

--- cript.js
+++
@@ -1,5 +0,0 @@
-[33m67bdf0c[m[33m ([m[1;36mHEAD[m[33m -> [m[1;32mmain[m[33m, [m[1;31morigin/main[m[33m, [m[1;31morigin/HEAD[m[33m)[m IRR graph
-[33md7a4875[m added mobile app and make it responsive
-[33m7d371e1[m ROI table and unclaimed depreciatoin fix
-[33mb785bc1[m changed ROI table for more clarity
-[33m39eefdf[m made export related changes
